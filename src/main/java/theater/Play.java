bbdc8490fa19d6d1c7c9b6331aebe9e0b833152c
gskahlon <gurmanatsinghkahlon@gmail.com>
2025-11-19 14:45:58 -0500

    completed task 1.1 by doing some minor refactoring like encapsucalting the instance vaariable, making constants final , adding javdoc documentations

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,33 @@
 package theater;
 
+/**
+ * This class stores basic information about a play,
+ * including its title and its genre or classification.
+ * It provides getters and setters for both fields.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
+        this.setName(name);
+        this.setType(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
